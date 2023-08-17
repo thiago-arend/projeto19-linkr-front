78bfe8a1ebf76916535c25e123628cbf72471da2
Matheus <matheusvmja@gmail.com>
2023-08-17 17:09:32 -0300

    Feat: Login finished

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,48 @@
-import axios from "axios";
-import { UserContext } from "../App.jsx";
-import { Link, useNavigate } from "react-router-dom";
-import { useContext, useEffect, useState } from "react";
-import { SingInContainer, TitleContainer, ContentContainer, FormContainer } from "./SignInPage/styled.js";
+import axios from "axios"
+import { UserContext } from "../App.jsx"
+import { Link, useNavigate } from "react-router-dom"
+import { useContext, useEffect, useState } from "react"
+import { SingInContainer, TitleContainer, ContentContainer, FormContainer } from "./SignInPage/styled.js"
 
 export default function SignInPage() {
+  const [login, setLogin] = useState({})
+  const navigate = useNavigate()
+  const { setToken, setUserId } = useContext(UserContext)
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false)
 
-  /* Variaveis de Estado */
-
-
-  /* Configurações da Página */
-  const navigate = useNavigate();
-  /*const url = import.meta.env.VITE_API_URL;*/
-  const User = useContext(UserContext).UserData;
-  const setUser = useContext(UserContext).SetUserData;
-
-  /* Token: existencia + uso */
-
-  /* Se houver um Token: */
   useEffect(() => {
+    if (localStorage.getItem("token")) navigate("/timeline")
+  }, [])
 
-  }, []);
+  const handleChange = (event) => {
+    const name = event.target.name
+    const value = event.target.value
+    setLogin(values => ({ ...values, [name]: value }))
+  }
 
+  const handleSubmit = async (event) => {
+    event.preventDefault()
 
+    setIsButtonDisabled(true)
 
+    try {
+      await signIn()
+    } finally {
+      setIsButtonDisabled(false)
+    }
+  }
+
+  function signIn() {
+    axios.post(`${process.env.REACT_APP_API_URL}/`, login)
+      .then(res => {
+        setToken(res.data.token)
+        setUserId(res.data.userId)
+        localStorage.setItem("token", res.data.token)
+        localStorage.setItem("user", res.data.userId)
+        navigate("/timeline")
+      })
+      .catch(err => alert(err.response.data))
+  }
 
   return (
     <SingInContainer>
@@ -38,27 +57,38 @@ export default function SignInPage() {
       </TitleContainer>
 
       <FormContainer>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input
+            data-test="email"
             required
             type="email"
             placeholder="E-mail"
             name="email"
+            value={login.email || ""}
+            onChange={handleChange}
           />
           <input
+            data-test="password"
             required
             type="password"
             placeholder="Password"
             name="password"
+            value={login.password || ""}
+            onChange={handleChange}
           />
-          <button type="submit">Log In</button>
+          <button
+            data-test="login-btn"
+            type="submit"
+            disabled={isButtonDisabled}
+          >Log In
+          </button>
         </form>
 
-        <Link to="signup">
+        <Link data-test="sign-up-link" to="/signup">
           First time? Create an account!
         </Link>
       </FormContainer>
 
     </SingInContainer>
-  );
+  )
 }
