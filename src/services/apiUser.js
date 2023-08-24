2d4c95e8185fd1dfccb77fc3457c4b72ce283406
thiago-arend <thiago.arend@gmail.com>
2023-08-23 23:16:11 -0300

    feat: botão de follow/unfollow

--- src/services/apiUser.js
+++ src/services/apiUser.js
@@ -9,5 +9,32 @@ function verifyFriendsExistence(token) {
     return promise;
 }
 
-const apiUser = { verifyFriendsExistence };
+function verifyFollowingUser(token, id) {
+    const auth = {
+        headers: { Authorization: `Bearer ${token}` }
+    };
+
+    const promise = axios.post(`${process.env.REACT_APP_API_URL}/user/is-following/${id}`, {}, auth);
+    return promise;
+}
+
+function followUser(token, id) {
+    const auth = {
+        headers: { Authorization: `Bearer ${token}` }
+    };
+
+    const promise = axios.post(`${process.env.REACT_APP_API_URL}/user/${id}/follow`, {}, auth);
+    return promise;
+}
+
+function unfollowUser(token, id) {
+    const auth = {
+        headers: { Authorization: `Bearer ${token}` }
+    };
+
+    const promise = axios.post(`${process.env.REACT_APP_API_URL}/user/${id}/unfollow`, {}, auth);
+    return promise;
+}
+
+const apiUser = { verifyFriendsExistence, verifyFollowingUser, followUser, unfollowUser };
 export default apiUser;
